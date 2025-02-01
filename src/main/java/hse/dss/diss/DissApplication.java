ff70e9ab29418401c4a3a38e1bd2c8fb82f0af93
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2025-02-02 01:14:41 +0500

    Initial commit

---
+++ src/main/java/hse/dss/diss/DissApplication.java
@@ -0,0 +1,13 @@
+package hse.dss.diss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DissApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DissApplication.class, args);
+	}
+
+}
