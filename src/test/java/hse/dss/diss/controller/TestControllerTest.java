63900b05e3fc8f20d30dd201c7b1084a3a648863
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2025-02-02 22:00:51 +0500

    fix: remove docker image pushing

--- src/test/java/hse/dss/diss/controller/TestControllerTest.java
+++ src/test/java/hse/dss/diss/controller/TestControllerTest.java
@@ -1,10 +1,26 @@
 package hse.dss.diss.controller;
 
+import hse.dss.diss.service.TestService;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.ActiveProfiles;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @ActiveProfiles("test")
 class TestControllerTest {
+
+    @InjectMocks
+    private TestController testController;
+
+    @Mock
+    private TestService testService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
 
 }
