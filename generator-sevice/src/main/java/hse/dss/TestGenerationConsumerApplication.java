b2eba9229e37c8b09db892a53a6b4d3163378574
AlexanderGarifullin <garifullin.alexandre@yandex.ru>
2025-03-10 00:44:05 +0500

    global: add microservices 2

---
+++ generator-sevice/src/main/java/hse/dss/TestGenerationConsumerApplication.java
@@ -0,0 +1,11 @@
+package hse.dss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGenerationConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TestGenerationConsumerApplication.class, args);
+    }
+}
---
+++ web-service/src/main/java/hse/dss/WebApplication.java
@@ -0,0 +1,13 @@
+package hse.dss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@SpringBootApplication
+@EnableAsync
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
